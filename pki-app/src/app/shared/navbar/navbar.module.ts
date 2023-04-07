503df073ac5310e9b6b6129f860233a0b973fdf6
Miloš Zeljko <miloszeljko00@gmail.com>
2023-04-07 20:51:00 +0200

    moved navbar module outside of ui

--- pki-app/src/app/shared/navbar/navbar.module.ts
+++ pki-app/src/app/shared/navbar/navbar.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarButtonComponent } from './components/navbar-button/navbar-button.component';
-import { MaterialModule } from '../../material/material.module';
+import { MaterialModule } from '../material/material.module';
 
 
 
